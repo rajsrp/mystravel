6a447964bb3a322fb76b49bc05c0eb579c01d149
ramya90 <ramya.siteurl@gmail.com>
2017-05-29 17:34:03 +0530

    removed css from coorg 3 days trip page

--- coorg.php
+++ coorg.php
@@ -7,124 +7,124 @@
 <!----------------Content Area------------------>
 
 
-<div class="w3-container w3-center" style="padding-top:40px; padding-bottom: 20px; width: 100%; align: center;">
-<span style="font-size: 30px; color: blue; font-family: arial; font-weight: bold; text-align: middle; padding-top:20px; padding-bottom: 35px;">PLACES TO VISIT IN 3 DAYS IN COORG</span>
+<div class="w3-container w3-center w3-padding w3-text-indigo">
+<h1><strong>PLACES TO VISIT IN 3 DAYS IN COORG</strong></h1>
 </div>
-<div class="w3-container" style="padding-top:10px; padding-bottom: 30px; width: 100%; margin: 10px;">
+<div class="w3-container w3-padding w3-margin">
 
- <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px;">
+ <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-   <img src="images/3daytrip36.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Raja's seat</h4>
+   <img src="images/3daytrip36.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Raja's seat</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip20.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Raja's Tomb</h4>
+  <img src="images/3daytrip20.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Raja's Tomb</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip14.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Bylakuppe</h4>
+  <img src="images/3daytrip14.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Bylakuppe</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip35.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Thalakaveri</h4>
+  <img src="images/3daytrip35.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Thalakaveri</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip38.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Irpu Falls</h4>
+  <img src="images/3daytrip38.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Irpu Falls</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip39.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Abbey Falls</h4>
+  <img src="images/3daytrip39.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Abbey Falls</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip40.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Sri Bhagandeshwara Temple</h4>
+  <img src="images/3daytrip40.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Sri Bhagandeshwara Temple</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip41.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Kavery Nisargadama</h4>
+  <img src="images/3daytrip41.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Kavery Nisargadama</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip37.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Dubare Elephant Camp</h4>
+  <img src="images/3daytrip37.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Dubare Elephant Camp</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip13.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Nagarahole National Park</h4>
+  <img src="images/3daytrip13.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Nagarahole National Park</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip14.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Golden Temple</h4>
+  <img src="images/3daytrip14.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Golden Temple</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip42.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Brahmagiri Hill</h4>
+  <img src="images/3daytrip42.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Brahmagiri Hill</h4>
     </div>
   </div>
 </div> 
 
-  <div class="w3-quarter" style="padding-left: 10px; padding-right: 10px; padding-top: 20px;">
+  <div class="w3-quarter w3-padding">
   <div class="w3-card-2">
-  <img src="images/3daytrip43.jpg" style="width:100%" class="w3-hover-opacity">
-    <div class="w3-container">
-      <h4 style="text-align: center;">Mallalli Falls</h4>
+  <img src="images/3daytrip43.jpg" width=100% class="w3-hover-opacity">
+    <div class="w3-container w3-center">
+      <h4>Mallalli Falls</h4>
     </div>
   </div>
 </div> 
