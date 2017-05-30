a7f09f371e8273a21571f404da90103f73e064c3
ramya90 <ramya.siteurl@gmail.com>
2017-05-30 10:52:11 +0530

    changes done about us page, alignment and keywords added

--- about-us.php
+++ about-us.php
@@ -25,7 +25,7 @@
 
 <!--<div class="w3-container w3-center" style="padding-top:40px; padding-bottom: 55px; width: 100%; align: center;">-->
 <div class="w3-container w3-center w3-padding w3-text-indigo">
-<h1><strong>ABOUT US</strong></h1>
+<span class="w3-xxlarge"><strong>ABOUT US</strong></span>
 </div>
 <div class="w3-container w3-padding w3-margin w3-left">
 Mysore Travel is the first choice of people planning for a vacation. We provide the best quality of services with all the facilities. We are asscociated with many hotels, homestays around the city in order to provide the best facilities you are looking for. Guides will be provided in case required. 
@@ -33,27 +33,31 @@ Mysore Travel is the first choice of people planning for a vacation. We provide 
 We are one of the top travel agencies in the city known for our best service, best price. 
 <br><br>
 
-We are offering you various packages in and around the city, to know more about the packages
+We are offering you various packages in and around the city with the lowest possible price.
 <br>
-<div class="w3-container w3-left w3-padding">
- <a href="packages.php" class="w3-btn w3-square-xxlarge w3-hover-white w3-indigo w3-hover-text-black"><strong>CLICK HERE</strong></a>
+<br>
 </div>
+
+<div class="w3-container w3-center w3-padding">
+<h1 class="w3-xlarge w3-center w3-padding-32">To know more about mysore sightseeing cab packages</h1>
+ <a href="packages.php" class="w3-btn w3-square-xxlarge w3-hover-white w3-indigo w3-hover-text-black w3-center"><strong>CLICK HERE</strong></a>
+</div>
+<div class="w3-padding-32">
 </div>
 <div class="w3-center">
 <h3>Few of the places you can visit.</h3>
-</div>
-<div class="slideshow-container">
+<div class="slideshow-container w3-margin">
   <div class="mySlides fade w3-padding w3-margin w3-center">
   <!--  <div class="numbertext">1 / 3</div>-->
-   <img src="images/slideimage.jpg">
+   <img src="images/slideimage.jpg" class="w3-center w3-padding w3-margin">
   </div>
 
   <div class="mySlides fade w3-padding w3-margin w3-center">
    <!-- <div class="numbertext">2 / 3</div>-->
-  <img src="images/slideimage2.jpg">
+  <img src="images/slideimage2.jpg" class="w3-center w3-padding w3-margin">
   </div>
   </div>
-  
+  </div>
   
   <br><br>
 
