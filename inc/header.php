c53041cc21f0fc720d8472cbe43eda298c84682a
Rajesh MR <rajesh.siteurl@gmail.com>
2017-05-31 17:31:40 +0530

    Edited wiki

--- inc/header.php
+++ inc/header.php
@@ -5,55 +5,36 @@
 <link rel="stylesheet" href="css/w3-colors-highway.css">
 
 <link rel="stylesheet" href="css/style.css">
-
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-2017.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-camo.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-vivid.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-windows.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <body>
 
-<div class="w3-bar w3-black">
-
-<a href="#" class="w3-bar-item  w3-button" >info@mysoretravel.com</a>
-<a href="#" class="w3-bar-item w3-button" >1234567890</a>
-</div>
-<!--<div class="w3-bar w3-indigo w3-padding">
-<div class="w3-left w3-margin-left">
-<a href="index.php" class="w3-left"><img src="images/mysoretraveldummylogo.png" alt="Mysore Travel" width="200" ></a>
- </div>
-  <div class="w3-right w3-padding">
-  <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-hover-text-blue w3-text-white" >Home</a>
-  <a href="tem-about-us.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-hover-text-blue w3-text-white" >About Us</a>
-  <a href="packages.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-hover-text-blue w3-text-white" >Packages</a>
-  <a href="tem-contact-us.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-hover-text-blue w3-text-white" >Contact Us</a>
-  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium w3-hover-white w3-hover-text-blue w3-text-white" onclick="myFunction()">&#9776;</a>
-</div>
+<div class="w3-bar w3-light-grey w3-padding ">
+<div class="w3-right">
+<a href="#" class="w3-bar-item  w3-button w3-border   w3-highway-orange" >info@mysoretravel.com</a>
+<a href="tel:+919448674775" class="w3-bar-item w3-button w3-border w3-margin-left w3-highway-orange" >Tap To Call Now</a>
 </div>
 </div>
 
-<div id="demo" class="w3-bar-block w3-red  w3-hide-large w3-hide-medium">
-  <a href="tem-about-us.php" class="w3-bar-item w3-button w3-hover-white w3-text-white" >About Us</a>
-  <a href="packages.php" class="w3-bar-item w3-button w3-hover-white w3-text-white" >Packages</a>
-  <a href="tem-contact-us.php" class="w3-bar-item w3-button w3-hover-white w3-text-white" >Contact Us</a>
-</div>-->
+<div class="w3-bar w3-highway-blue w3-padding">
+<a href="index.php" class="w3-bar-item"><img src="images/logo.png" width="170"></a>
 
-<div class="w3-bar w3-indigo">
-<a href="index.php" class="w3-bar-item"><img src="images/logo.png" width="200"></a>
-<!--<<<<<<< HEAD-->
 <div class="w3-right w3-padding">
   <a href="index.php" class="w3-bar-item w3-button w3-hide-small">Home</a>
   <a href="about-us.php" class="w3-bar-item w3-button w3-hide-small">About Us</a>
   <a href="packages.php" class="w3-bar-item w3-button w3-hide-small">Packages</a>
   <a href="contact-us.php" class="w3-bar-item w3-button w3-hide-small">Contact Us</a></div>
-<!--=======
-<div class="w3-right w3-padding">
-  <a href="#" class="w3-bar-item w3-button w3-hide-small">Home</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 3</a></div>
->>>>>>> 6dfa74a1b2f5f023764e8a3645b0755be42fbc2e-->
-  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="myFunction()">&#9776;</a>
-</div>-
 
-<div id="demo" class="w3-bar-block w3-red w3-hide w3-hide-large w3-hide-medium">
+
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="myFunction()">&#9776;</a>
+</div>
+
+<div id="demo" class="w3-bar-block w3-highway-red w3-hide w3-hide-large w3-hide-medium">
  <a href="about-us.php" class="w3-bar-item w3-button">About Us</a>
   <a href="packages.php" class="w3-bar-item w3-button">Packages</a>
   <a href="contact-us.php" class="w3-bar-item w3-button">Contact Us</a>
---
+++ mystravel.wiki/Home.md
@@ -0,0 +1,16 @@
+Welcome to the mystravel wiki!
+
+1. About Mysore
+2. Get in 
+3. Get around
+4. Sightseeing Places in  Mysore
+5. What you can do in Mysore.
+6. What you can buy in Mysore.
+7. Special dishes of Mysore
+8. Hotels in Mysore
+	* Veg 
+	* Nonveg
+9. Lodges in Mysore
+10. Sightseeing places around Mysore
+11. Tips about Mysore
+
